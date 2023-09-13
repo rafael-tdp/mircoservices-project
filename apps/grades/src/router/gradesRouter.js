7d19b01a7e06f57fbeacea70a776dd76cd3eaaf5
Rafael93150 <tavaresrafael93@gmail.com>
2023-09-13 08:35:21 +0200

    add getUserGrades et addUserGrade

--- apps/grades/src/router/gradesRouter.js
+++ apps/grades/src/router/gradesRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getUserGrades } from "../routes/gradesRoutes.js";
+import { getUserGrades, addUserGrade } from "../routes/gradesRoutes.js";
 
 const router = express.Router();
-const app = express();
 
 router.get("/grades/:userId", getUserGrades);
+router.post("/grades/:userId", addUserGrade);
 
 export default router;
--- apps/grades/src/routes/gradesRoutes.js
+++ apps/grades/src/routes/gradesRoutes.js
@@ -4,11 +4,72 @@ import { Types } from "mongoose";
 export const getUserGrades = async (req, res) => {
     try {
         const userId = req.params.userId;
-        const grades = await Grade.find({ user: new Types.ObjectId(userId) });
-        res.json(grades);
+        // const grades = await Grade.find({ user: new Types.ObjectId(userId) });
+
+        const pipeline = [
+            {
+                $match: {
+                    user: new Types.ObjectId(userId),
+                },
+            },
+            {
+                $lookup: {
+                    from: "subjects",
+                    localField: "subject",
+                    foreignField: "_id",
+                    as: "subjectInfo",
+                },
+            },
+            {
+                $unwind: "$subjectInfo",
+            },
+            {
+                $group: {
+                    _id: "$subjectInfo.name",
+                    grades: {
+                        $push: "$grade",
+                    },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    subject: "$_id",
+                    grades: 1,
+                },
+            },
+        ];
+
+        const gradesBySubject = await Grade.aggregate(pipeline);
+
+        res.json(gradesBySubject);
     } catch (error) {
         res.status(500).json({
             error: `Une erreur est survenue lors de la récupération des notes de l'utilisateur : ${error}`,
+        });
+    }
+};
+
+export const addUserGrade = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const { subjectId, grade } = req.body;
+
+        const newGrade = new Grade({
+            user: userId,
+            subject: subjectId,
+            grade: grade,
+        });
+
+        await newGrade.save();
+
+        res.json({
+            message: "Note ajoutée avec succès",
+            grade: newGrade,
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: `Une erreur est survenue lors de l'ajout d'une note à l'utilisateur : ${error}`,
         });
     }
 };
