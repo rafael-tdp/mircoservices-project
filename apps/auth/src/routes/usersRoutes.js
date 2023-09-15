372ab0a651ae946f0db07eb75de5bfd8e91e0e87
Rafael93150 <tavaresrafael93@gmail.com>
2023-09-15 14:14:42 +0200

    add getUsers route

--- apps/auth/src/routes/usersRoutes.js
+++ apps/auth/src/routes/usersRoutes.js
@@ -1,5 +1,16 @@
 import User from "../models/user.js";
 
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({}, { password: 0 });
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({
+            error: `Une erreur est survenue lors de la récupération des utilisateurs : ${error}`,
+        });
+    }
+};
+
 export const addTeacherInfoToGrades = async (req, res) => {
     try {
         const grades = req.body;
